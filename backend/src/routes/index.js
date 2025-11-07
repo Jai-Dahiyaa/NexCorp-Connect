4d987bfb12fdd1e3f61f06b5d534e8e55fa5df42
Jai-Dahiyaa <virdahiya6@gmail.com>
2025-11-07 13:30:42 +0530

    final auth test code and oauth api test

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,7 @@
 import signUp from "./localAuth.routes.js";
+import oauth from "./OAuth.routes.js"
 
 export default function registerRoutes (app) {
   app.use('/auth', signUp);
+  app.use('/api/v1/oauth/', oauth)
 };

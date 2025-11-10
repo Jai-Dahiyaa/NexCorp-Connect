e0b0a0848f06914f102718746c4d54315fc07ac0
Jai-Dahiyaa <virdahiya6@gmail.com>
2025-11-10 17:58:27 +0530

    jest setup file code change

--- backend/jest.setup.js
+++ backend/jest.setup.js
@@ -1,20 +1,22 @@
-jest.mock('pg', () => {
-  const mClient = {
-    connect: jest.fn(),
-    query: jest.fn().mockResolvedValue({ rows: [] }),
-    end: jest.fn(),
-  };
-  return { Client: jest.fn(() => mClient) };
-});
-
-jest.mock('redis', () => {
-  return {
-    createClient: jest.fn(() => ({
+beforeAll(() => {
+  jest.mock('pg', () => {
+    const mClient = {
       connect: jest.fn(),
-      on: jest.fn(),
-      get: jest.fn(),
-      set: jest.fn(),
-      quit: jest.fn(),
-    })),
-  };
+      query: jest.fn().mockResolvedValue({ rows: [] }),
+      end: jest.fn(),
+    };
+    return { Client: jest.fn(() => mClient) };
+  });
+
+  jest.mock('redis', () => {
+    return {
+      createClient: jest.fn(() => ({
+        connect: jest.fn(),
+        on: jest.fn(),
+        get: jest.fn(),
+        set: jest.fn(),
+        quit: jest.fn(),
+      })),
+    };
+  });
 });

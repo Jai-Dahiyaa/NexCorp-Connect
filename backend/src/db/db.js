2eacc96771f5aa49290b91b1317c6a23cacad02d
Jai-Dahiyaa <virdahiya6@gmail.com>
2026-01-18 23:21:06 +0530

    Day: 18:01:2026

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -8,11 +8,7 @@ let pool = null;
 
 if (process.env.NODE_ENV !== 'test') {
   pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
   });
 
   pool.connect((err) => {

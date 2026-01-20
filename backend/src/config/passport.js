6f102a5ff187e5c2e3bccf693eb9d61d79d20b78
Jai-Dahiyaa <virdahiya6@gmail.com>
2026-01-20 21:18:23 +0530

    DELETE: Remove linkedin oauth link

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -1,7 +1,6 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { Strategy as GitHubStrategy } from 'passport-github2';
-import { Strategy as LinkedInStrategy } from 'passport-linkedin-oauth2';
 
 const commonCallback = async (accessToken, refreshToken, profile, done) => {
   try {

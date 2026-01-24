fdbb01a66ccbf8670e62d118450fd108497b44fb
Jai-Dahiyaa <virdahiya6@gmail.com>
2026-01-24 23:39:43 +0530

    REFECTOR: Add other connection redis for jest

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,26 +1,28 @@
-import logger from "../config/logger.js";
-import pkg from "redis";
+import logger from '../config/logger.js';
+import pkg from 'redis';
 const { createClient } = pkg;
 
 const redisClient = createClient({
   url: process.env.REDIS_URL,
 });
 
-redisClient.on("error", (err) => {
+redisClient.on('error', (err) => {
   logger.error(`Redis connection error: ${err?.message || err}`);
 });
 
-redisClient.on("connect", () => {
-  logger.info("Redis client connected successfully");
+redisClient.on('connect', () => {
+  logger.info('Redis client connected successfully');
 });
 
-redisClient.on("ready", () => {
-  logger.info("Redis is ready to use");
+redisClient.on('ready', () => {
+  logger.info('Redis is ready to use');
 });
 
 async function connectRedis() {
   try {
-    await redisClient.connect();
+    if (!redisClient.isOpen) {
+      await redisClient.connect();
+    }
   } catch (err) {
     logger.error(`Redis initial connect failed: ${err?.message || err}`);
     setTimeout(connectRedis, 2000);
@@ -30,3 +32,9 @@ async function connectRedis() {
 connectRedis();
 
 export default redisClient;
+
+export async function closeRedis() {
+  if (redisClient.isOpen) {
+    await redisClient.quit();
+  }
+}

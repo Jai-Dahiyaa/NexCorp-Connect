d4df99bfc26f362c61648c673a0469767cfdc16a
Sanket Dahiya <virdahiya6@gmail.com>
2026-02-22 06:27:25 +0000

    ADD: APIs rate limit config add

---
+++ backend/src/config/rateLimit.js
@@ -0,0 +1,17 @@
+import rateLimit from "express-rate-limit";
+import RedisStore from "rate-limit-redis";
+import redisClient from "./redis.js"
+
+export const limiter = rateLimit({
+    store: new RedisStore({
+        sendCommand: (...args) => redisClient.sendCommand(args),
+    }),
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: {
+        status: false,
+        message: "Too many request, please try again later"
+    }
+})

078b7d47e48fcc0d40e05398b43b4343de091bb8
Sanket Dahiya <virdahiya6@gmail.com>
2026-02-24 21:39:58 +0000

    REFECTOR: Rewrite code redis without auth connect

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,21 +1,21 @@
-import logger from '../config/logger.js';
-import pkg from 'redis';
+import logger from "../config/logger.js";
+import pkg from "redis";
 const { createClient } = pkg;
 
 const redisClient = createClient({
   url: process.env.REDIS_URL,
 });
 
-redisClient.on('error', (err) => {
+redisClient.on("error", (err) => {
   logger.error(`Redis connection error: ${err?.message || err}`);
 });
 
-redisClient.on('connect', () => {
-  logger.info('Redis client connected successfully');
+redisClient.on("connect", () => {
+  logger.info("Redis client connected successfully");
 });
 
-redisClient.on('ready', () => {
-  logger.info('Redis is ready to use');
+redisClient.on("ready", () => {
+  logger.info("Redis is ready to use");
 });
 
 async function connectRedis() {

57faab4f4801589a83ec07aa9ba323f1228964ee
Sanket Dahiya <virdahiya6@gmail.com>
2026-02-27 12:51:05 +0000

    FIX: Again config jest file for test code

--- backend/jest.config.mjs
+++ backend/jest.config.mjs
@@ -1,12 +1,12 @@
 export default {
-  testEnvironment: "node",
-  testTimeout: 30000,
-  verbose: true,
-  transform: {},
+  testEnvironment: "node",          
+  testTimeout: 30000,               
+  verbose: true,                    
+  transform: {},                    
   moduleFileExtensions: ["js", "mjs"],
-  testMatch: ["<rootDir>/tests/**/*.test.js"],
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  forceExit: true,
-  detectOpenHandles: true
+  testMatch: ["<rootDir>/tests/**/*.test.js"], 
+  collectCoverage: true,            
+  coverageDirectory: "coverage",    
+  forceExit: true,                  
+  detectOpenHandles: true           
 };
